76385b6d29a590d7fa19fc855c47b1e93af46c99
ga.zhang <1299407509@qq.com>
2020-02-24 12:45:39 +0800

    update

--- wechatservice-netty/src/main/java/com/corereach/communication/netty/handler/HeartBeatHandler.java
+++ wechatservice-netty/src/main/java/com/corereach/communication/netty/handler/HeartBeatHandler.java
@@ -4,7 +4,6 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -27,33 +26,22 @@ public class HeartBeatHandler extends ChannelInboundHandlerAdapter {
         if (evt instanceof IdleStateEvent) {
             /**强制类型转换*/
             IdleStateEvent event = (IdleStateEvent) evt;
-//            switch (event.state()) {
-//                case READER_IDLE:
-//                    log.info("进入读空闲");
-//                    break;
-//                case WRITER_IDLE:
-//                    log.info("进入写空闲");
-//                    break;
-//                case ALL_IDLE:
-//                    log.info("channel关闭前，users数量为：{}",WebSocketChatHandler.users.size());
-//                    Channel channel = ctx.channel();
-//                    /**关闭无用的channel，以防资源浪费*/
-//                    channel.close();
-//                    log.info("channel关闭后，users数量为：{}",WebSocketChatHandler.users.size());
-//                    break;
-//                default:
-//                    break;
-//            }
-            if (event.state() == IdleState.READER_IDLE) {
-                log.info("进入读空闲");
-            } else if (event.state() == IdleState.WRITER_IDLE) {
-                log.info("进入写空闲");
-            } else if (event.state() == IdleState.ALL_IDLE) {
-                log.info("channel关闭前，users数量为：{}", WebSocketChatHandler.users.size());
-                Channel channel = ctx.channel();
-                /**关闭无用的channel，以防资源浪费*/
-                channel.close();
-                log.info("channel关闭后，users数量为：{}", WebSocketChatHandler.users.size());
+            switch (event.state()) {
+                case READER_IDLE:
+                    log.info("进入读空闲");
+                    break;
+                case WRITER_IDLE:
+                    log.info("进入写空闲");
+                    break;
+                case ALL_IDLE:
+                    log.info("channel关闭前，users数量为：{}",WebSocketChatHandler.users.size());
+                    Channel channel = ctx.channel();
+                    /**关闭无用的channel，以防资源浪费*/
+                    channel.close();
+                    log.info("channel关闭后，users数量为：{}",WebSocketChatHandler.users.size());
+                    break;
+                default:
+                    break;
             }
         }
     }
